4d7f301df25be6b59435c68fe2e2da7d8219dcbd
singh-sudhir16 <mail.sudhir.singh14@gmail.com>
2024-08-19 09:19:19 +0530

    Fucked up commit

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -5,6 +5,7 @@ import { useBalance } from "@repo/store/balance";
 export default function() {
   const balance = useBalance();
   return <div>
+    <p> Change made in the merchant app</p>
     YOur current balance is this :  {balance} . Have a good time .
   </div>
 }
