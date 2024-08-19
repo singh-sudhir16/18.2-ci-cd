39f9a9b3bdfdb9fadf0db295ad0723026daf25ab
singh-sudhir16 <mail.sudhir.singh14@gmail.com>
2024-08-19 21:57:02 +0530

    Demo of CI-CD

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -6,6 +6,7 @@ export default function() {
   const balance = useBalance();
   return <div>
     <p> Again making changes in different branch</p>
+    <p> This is the final change to show the CI-CD demo that's why we are making some changes in different branch of same repo</p>
     YOur current balance is :  {balance} . Have a good time .
   </div>
 }
