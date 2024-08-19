4dff5354669f54cae3741af9e1a9a9cd7d305ff3
singh-sudhir16 <mail.sudhir.singh14@gmail.com>
2024-08-19 09:21:10 +0530

    Updated apps/merchant-app/app/page.tsx

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -5,6 +5,7 @@ import { useBalance } from "@repo/store/balance";
 export default function() {
   const balance = useBalance();
   return <div>
+    <p> Again making changes in different branch</p>
     YOur current balance is :  {balance} . Have a good time .
   </div>
 }
